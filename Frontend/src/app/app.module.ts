41674f7029f87278ba9c8bdff7b3f29d00dc6c1d
kadevmaster <nguyenvanka1994@gmail.com>
2021-02-26 08:59:58 +0700

    template Driven Forms

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from "./app.component";
 import { PropertyCardComponent } from "./property/property-card/property-card.component";
 import { PropertyListComponent } from "./property/property-list/property-list.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import {FormsModule} from "@angular/forms";
 
 import { HousingService } from "./services/housing.service";
 import { Routes, RouterModule } from "@angular/router";
@@ -40,7 +41,7 @@ const appRoutes: Routes = [
     AddPropertyComponent,
     PropertyDetailComponent
   ],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, HttpClientModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [HousingService],
   bootstrap: [AppComponent],
 })
--- Frontend/src/app/property/AddProperty/AddProperty.component.ts
+++ Frontend/src/app/property/AddProperty/AddProperty.component.ts
@@ -1,13 +1,16 @@
+import { from } from 'rxjs';
 import { Router} from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-AddProperty',
+  selector: 'app-Add-Property',
   templateUrl: './AddProperty.component.html',
   styleUrls: ['./AddProperty.component.scss']
 })
 export class AddPropertyComponent implements OnInit {
 
+  @ViewChild('Form') addPropertyForm: NgForm;
   constructor(private router : Router) { }
 
   ngOnInit() {
@@ -15,6 +18,12 @@ export class AddPropertyComponent implements OnInit {
 
   onBack(){
     this.router.navigate(['/'])
+  }
+
+  onSubmit()
+  {
+console.log("show some thing");
+console.log(this.addPropertyForm);
   }
 
 }
