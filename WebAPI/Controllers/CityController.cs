7ad7b224e3b5f29e6b2a3b813960e8225aadc55e
kadevmaster <nguyenvanka1994@gmail.com>
2021-03-01 12:32:47 +0700

    create WebAPI

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+        }
+
+        [HttpGet("")]
+        public IEnumerable<string> GetString()
+        {
+            return new string[] {"gia nghia", "dak min"};
+        }
+    }
+}
